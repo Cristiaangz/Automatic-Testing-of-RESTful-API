18f0b27fcf03adc3c264a7d889dcfa36a0ed8e84
cristiaangz <cristiaangz@gmail.com>
2021-04-08 14:53:30 +0400

    Integrates POST positive tests into a single test.
    Replaced the status code and payload POST positive tests by a single
    basic POST test. The payload is now a global variable called
    valid_payload to reduce clutter and increase readability.

--- gorest_test/test_positive.py
+++ gorest_test/test_positive.py
@@ -11,9 +11,17 @@ def setup_module(module):
     global user_endpoint
     global user_123_endpoint
     global token
+    global valid_payload
     main_url = "https://gorest.co.in"
     user_endpoint = "/public-api/users"
     user_123_endpoint = "/public-api/users/123"
+    valid_payload = {
+            "name": "Dan Doney",
+            "email": "dan@securrency.com",
+            "gender" : "Male",
+            "status" : "Active",
+        }
+    
     if os.path.isfile("token.txt"):
         f = open("token.txt", "r")
         token = f.readline()
@@ -51,7 +59,7 @@ class Test_GET_User:
 
         assert not errors, "Errors Occured:\n{}".format("\n".join(errors))
     
-    def test_idempotency(self):
+    def test_GET_idempotency(self):
         errors = []
         url = main_url + user_endpoint
         response1 = requests.get(main_url + user_endpoint)
@@ -62,60 +70,36 @@ class Test_GET_User:
 
 class Test_POST_User:
 
-    def test_statuscode(self):
+    def test_POST_basic(self):
         errors = []
         url = main_url + user_endpoint
         access_token_header = "Bearer " + token
-        payload = {
-            "name": "Dan Doney",
-            "email": "dan@securrency.com",
-            "gender" : "Male",
-            "status" : "Active",
-        }
-        response = requests.post(
-            url = url,
-            data = payload,
-            headers={"Authorization": access_token_header})
-        response_dict = is_proper_json(response)
-        if response_dict != False:
-            
-            if response_dict['code'] != 201:
-                print(response.status_code)
-                print(response.text)
-                errors.append("Response Access Code is {}.Expected 201"\
-                    .format(response.status_code))
-            
-            user_id = str(response_dict['data']['id'])
-            if not user_resource_delete(url = url+"/"+user_id, token = token):
-                errors.append('Failed to delete created user (ID: {})'.format(user_id))
-        else:
-            errors.append("Received a malformed JSON")
-            
-        assert not errors, "Errors Occured:\n{}".format("\n".join(errors))
-
-    def test_payload(self):
-        errors = []
-        url = main_url + user_endpoint
-        access_token_header = "Bearer " + token
-        payload = {
-            "name": "Dan Doney",
-            "email": "dan@securrency.com",
-            "gender" : "Male",
-            "status" : "Active",
-        }
         response_post = requests.post(
             url = url,
-            data = payload,
+            data = valid_payload,
             headers={"Authorization": access_token_header})
         
-        response_post_dict = response_post.json()
-        user_id = str(response_post_dict['data']['id'])
-        user_url = url + "/" + user_id
-        response_get = requests.get(url= user_url)
-        if not correctly_posted_user(response_post_dict['data'],payload):
-            errors.append('There is a discrepancy between the database and the sent payload')
-        if not user_resource_delete(url = url+"/"+user_id, token = token):
-                errors.append('Failed to delete created user (ID: {})'.format(user_id))
+        if response_post.status_code != requests.codes.ok:
+            errors.append("Status Code Error: received {} code".format(response_post.status_code))
+        response_post_dict = is_proper_json(response_post)
+        if response_post_dict != False:
+            
+            if response_post_dict['code'] != 201:\
+                errors.append("Response Access Code is {}.Expected 201"\
+                    .format(response_post_dict['code']))
+            
+            user_id = str(response_post_dict['data']['id'])
+            user_url = url + "/" + user_id
+            response_get = requests.get(url= user_url)
+            response_get_dict = response_get.json()
+            if not correctly_posted_user(response_get_dict['data'],valid_payload):
+                errors.append('Payload Error: User data in database and payload is not the same')
+            
+            if not user_resource_delete(url = user_url, token = token):
+                errors.append('Clean Up Error: Failed to delete created user (ID: {})'.format(user_id))
+        else:
+            errors.append("Payload Error: Received a malformed JSON")
+            
         assert not errors, "Errors Occured:\n{}".format("\n".join(errors))
     
     def test_state(self):
